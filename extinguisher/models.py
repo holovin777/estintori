b709700dd0c39574e4e450d4e13b43226ef82846
Viktor Holovin <holovin@mail.com>
2025-03-23 17:54:27 +0100

    Add Italian translation

--- extinguisher/models.py
+++ extinguisher/models.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from django.db import models
 from owner.models import Owner
 
@@ -9,17 +11,17 @@ class FireExtinguisher(models.Model):
         ('FOAM', 'AFFF Foam'),
     ]
 
-    code = models.CharField(max_length=20, unique=True, verbose_name="Extinguisher Code")
-    type = models.CharField(max_length=20, choices=TYPES, verbose_name="Extinguisher Type")
-    net_weight = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Net Weight (kg)")
-    manufacture_date = models.DateField(verbose_name="Manufacture Date")
-    revision_expiry_date = models.DateField(verbose_name="Revision Expiry Date")
-    test_expiry_date = models.DateField(verbose_name="Test Expiry Date")
-    next_maintenance_date = models.DateField(verbose_name="Next Maintenance Date")
-    location = models.CharField(max_length=100, verbose_name="Location")
-    active = models.BooleanField(default=True, verbose_name="Active/Inactive")
+    code = models.CharField(max_length=20, unique=True, verbose_name=_("Extinguisher Code"))
+    type = models.CharField(max_length=20, choices=TYPES, verbose_name=_("Extinguisher Type"))
+    net_weight = models.DecimalField(max_digits=5, decimal_places=2, verbose_name=_("Net Weight (kg)"))
+    manufacture_date = models.DateField(verbose_name=_("Manufacture Date"))
+    revision_expiry_date = models.DateField(verbose_name=_("Revision Expiry Date"))
+    test_expiry_date = models.DateField(verbose_name=_("Test Expiry Date"))
+    next_maintenance_date = models.DateField(verbose_name=_("Next Maintenance Date"))
+    location = models.CharField(max_length=100, verbose_name=_("Location"))
+    active = models.BooleanField(default=True, verbose_name=_("Active/Inactive"))
 
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, verbose_name="Owner")
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, verbose_name=_("Owner"))
 
     def __str__(self):
         return f"{self.code} - ({self.type})"
